bf49b92c9697cd70f175a239da8e1b17dc13193f
Justin Calderon <35749594+justin1571@users.noreply.github.com>
2018-04-30 17:44:40 -0700

    Add files via upload

--- LEDThread.cpp
+++ LEDThread.cpp
@@ -1,41 +1,4 @@
 /**
- * Copyright (c) 2017, Autonomous Networks Research Group. All rights reserved.
- * Developed by:
- * Autonomous Networks Research Group (ANRG)
- * University of Southern California
- * http://anrg.usc.edu/
- *
- * Contributors:
- * Jason A. Tran <jasontra@usc.edu>
- * Bhaskar Krishnamachari <bkrishna@usc.edu>
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy 
- * of this software and associated documentation files (the "Software"), to deal
- * with the Software without restriction, including without limitation the 
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
- * sell copies of the Software, and to permit persons to whom the Software is 
- * furnished to do so, subject to the following conditions:
- * - Redistributions of source code must retain the above copyright notice, this
- *     list of conditions and the following disclaimers.
- * - Redistributions in binary form must reproduce the above copyright notice, 
- *     this list of conditions and the following disclaimers in the 
- *     documentation and/or other materials provided with the distribution.
- * - Neither the names of Autonomous Networks Research Group, nor University of 
- *     Southern California, nor the names of its contributors may be used to 
- *     endorse or promote products derived from this Software without specific 
- *     prior written permission.
- * - A citation to the Autonomous Networks Research Group must be included in 
- *     any publications benefiting from the use of the Software.
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
- * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH 
- * THE SOFTWARE.
- */
-
-/**
  * @file       LEDThread.cpp
  * @brief      Implementation of thread that handles LED requests.
  *
@@ -73,38 +36,22 @@ void LEDThread(void *args)
 
             /* the second byte in the message denotes the action type */
             switch (msg->content[1]) {
-                case LED_THR_PUBLISH_MSG:
-                    printf("LEDThread: received command to publish to topic"
-                           "m3pi-mqtt-example/led-thread\n");
-                    pub_buf[0] = 'h';
-                    pub_buf[1] = 'i';
-                    message.qos = MQTT::QOS0;
-                    message.retained = false;
-                    message.dup = false;
-                    message.payload = (void*)pub_buf;
-                    message.payloadlen = 2; //MQTTclient.h takes care of adding null char?
-                    /* Lock the global MQTT mutex before publishing */
-                    mqttMtx.lock();
-                    client->publish(topic, message);
-                    mqttMtx.unlock();
-                    break;
-                case LED_ON_ONE_SEC:
-                    printf("LEDThread: received message to turn LED2 on for"
-                           "one second...\n");
-                    led2 = 1;
-                    wait(1);
-                    led2 = 0;
-                    break;
-                case LED_BLINK_FAST:
-                    printf("LEDThread: received message to blink LED2 fast for"
-                           "one second...\n");
-                    for(int i = 0; i < 10; i++)
-                    {
-                        led2 = !led2;
-                        wait(0.1);
-                    }
-                    led2 = 0;
-                    break;
+                
+                case "Forward":
+					m3pi.forward(25);
+					break; 
+				case "Left":
+					m3pi.left(25);
+					break;
+				case "Reverse":
+					m3pi.backward(25);
+					break;
+				case "Right":
+					m3pi.right(25);
+					break;
+				case "Stop":
+					m3pi.stop();
+					break;
                 default:
                     printf("LEDThread: invalid message\n");
                     break;
@@ -121,5 +68,3 @@ Mail<MailMsg, LEDTHREAD_MAILBOX_SIZE> *getLEDThreadMailbox()
 {
     return &LEDMailbox;
 }
-
-
